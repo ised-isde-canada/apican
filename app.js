5ef616ca705f334c6b60d068ff6cf74a2e62bb26
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2020-01-30 19:00:34 -0500

    getting dependencies, building blueprint of app

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 
 /*******************************************************************************
- * Franck Binard, ISED
- * Canadian Gov. API Store Management Software 2020
- *
+ * Franck Binard, Innovation Science and Economic Development Canada (ISED)
+ * franck.binard@canada.ca
+ * 
+ * Canadian Gov. API Store Management Software - written in 2020
  * Application APICan
- * -------------------------------------
+ * Manages GoC API Store (https://api.canada.ca/)
+ * -----------------------------------------------------------------------------
  *  app.js : application entry point, doesn't export anything
  *
- * server code compile with browserify into public/javascripts/bundle.js
- * view system is handlebars (see src/server/viewEngine.js)
+ * Front-end: 
+ * - js code compiles with browserify into public/javascripts/bundle.js
+ * - js code for front-end is in ./src/client, uses jquery and 
+ *   a other front-end libraries (see ./view/partials/head/jsLibraries)
+ * - template engine is handlebars, templates are stored in ./views
  *
+ * Back-end: 
+ * - uses an express stack
+ * - mongodb database
+ *
+ * Security: 
+ * - does not store any user information
+ * - only accepts requests from whitelisted ips
  ******************************************************************************/
-
 "use strict"
 
 /*****************************************************************************/
@@ -20,6 +31,7 @@ require('module-alias/register')    //uses aliases to express paths to modules i
 /*****************************************************************************/
 const appFeatures   = require('@apiCan/appfeatures').appFeatures
 const errors        = require('@errors/error').errors
+const report        = require('@report/report').report
 const security      = require('@security/security').security 
 /*****************************************************************************/
 
@@ -28,24 +40,28 @@ const appStatus = require('@apiCan/appStatus').appStatus({
     security, 
     errors
 }                
-
 /*****************************************************************************/
+
 const path = require('path')
 const httpServer    = ('@server/httpServer').httpServer({
     rootPath    : path.join( __dirname ) 
     viewPath    : path.join( __dirname, 'views' ) 
     staticPath  : path.join( __dirname, 'public')
 })
+
+/*****************************************************************************/
 const apiCanApp     = require('@apiCan/apiCanApp').apiCanApp( {
     httpServer, 
     appStatus
 }) 
 
+/*****************************************************************************/
 const appData       = require('@apiCan/appData').appData
 appData.spark( appStatus )                                     //get the app's configuration data
-.then( bootReport => apiCanApp.boot( bootReport ))  //start
-.then( bootReport => apiCanApp.run( bootReport ))   //application run
-.catch ( errors.handler )
-/*****************************************************************************/
-/*****************************************************************************/
-/*****************************************************************************/
+    .then( bootReport => apiCanApp.boot( bootReport ))  //start
+    .then( bootReport => apiCanApp.run( bootReport ))   //application run
+    .catch ( errors.handler )
+/******************************************************************************/
+/******************************************************************************
+ * end entry point
+ ******************************************************************************/
