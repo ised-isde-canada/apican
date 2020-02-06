19a1e043d108f68cbf4b9338166547e18d3e9b45
Franck Binard <franckbinard@gmail.com>
2020-02-05 19:08:40 -0500

    Update appData.js

--- src/apiCan/appData.js
+++ src/apiCan/appData.js
@@ -1,3 +1,7 @@
+/*****************************************************************************
+*
+*
+******************************************************************************/
 "use strict"
 
 /*****************************************************************************/
@@ -5,16 +9,22 @@ const config    = require('config')
 /*****************************************************************************/
 const appData = (function(){
 
-    let _keycloakClientID       = config.get('keycloakClientID')
-    let _keycloakClientSecret   = config.get('keycloakClientSecret')
-    let _keycloakClientDomain   = config.get('keycloakClientDomain')
-    let _settingsDBPath         = config.get('settingsDBPath')
-    let _appSecret              = config.get('appSecret')
+    let _keycloakClientID           = config.get('keycloakClientID')
+    let _keycloakClientSecret       = config.get('keycloakClientSecret')
+    let _keycloakClientDomain       = config.get('keycloakClientDomain')
+    let _settingsDBPath             = config.get('settingsDBPath')
+    let _appSecret                  = config.get('appSecret')
+    let _appBaseURL                 = config.get('appBaseURL')
     
     return {
         settingsDBPath   : _settingsDBPath, 
-        baseURL          : "http://localhost:3000/logout/callback", 
-        appSecret        : _appSecret
+        
+        appBaseURL          : _baseURL, 
+        appSecret           : _appSecret, 
+        
+        keycloakClientID        : _keycloakClientID, 
+        keycloakClientSecret    : _keycloakClientSecret, 
+        keycloakClientDomain    : _keycloakClientDomain
     }
 
 })()
